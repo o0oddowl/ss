48b5c29d0133aa2775c559b9e3dce1ca3656b9f2
o0oddowl <artemtop18@gmail.com>
2024-11-21 16:31:56 +0200

    s

--- src/component/form/form.jsx
+++ src/component/form/form.jsx
@@ -1,21 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import './form.css'
 import {useTelegram} from "../../hoock/useTelegram"
 const Form = () => {
     const {tg} = useTelegram()
+    const [country, setCountry] = useState('')
+    const onChangee = (e) => {
+        setCountry(e, target.value)
+    }
+    const onSendData = useCallback(()=>{
+        const data = {
+            country
+        }
+        tg.sendData(JSON.stringify(data))
+    }, [country])
+    useEffect(()=>{
+        tg.onEvent('mainButtonClicked', onSendData)
+        return() => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    },[onSendData])
+
 
     useEffect(() =>{
-        tg.MainButton.show()
         tg.MainButton.setParams({
             text: 'Тик-Тик'
         })
     }, [])
 
+    useEffect(() =>{
+        if(!country){
+            tg.MainButton.hide()
+        } else{
+            tg.MainButton.show()
+        }
+    }, [country])
+
 
     return(
         <div className={"form"}>
             <h3>Введінь дані</h3>
-            <input className={"input"} type="text" placeholder={"Країна"} />
+            <input className={"input"} 
+            type="text" 
+            placeholder={"Країна"} 
+            value={country} 
+            onChange={onChangee} />
             <select>
                 <option value={'legal'}>
                     Юр. лице
