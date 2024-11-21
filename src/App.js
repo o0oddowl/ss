5fb3084704be0b39ee5425e03b685f993a642dbd
o0oddowl <artemtop18@gmail.com>
2024-11-21 15:45:52 +0200

    s

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import './App.css';
 import {Route, Routes } from 'react-router-dom';
 import ProductList from './component/productList/ProductList';
 import Form from './component/form/form';
-const tg = window.Telegram.WebApp
+
 
 function App() {
-  const onClose = () => {
-  }
+
   return (
     <div className="App">
       <header />
