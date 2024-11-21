19640067477806664ae524a9d347b126dca66240
o0oddowl <artemtop18@gmail.com>
2024-11-21 16:54:00 +0200

    dd

--- src/hoock/useTelegram.js
+++ src/hoock/useTelegram.js
@@ -1,8 +1,15 @@
-const tg = window.Telegram.WebApp
-export function useTelegram(){
-    const onClose = () =>{
-        tg.close()
-    }
+//const tg = window.Telegram.WebApp
+//export function useTelegram(){
+   // const onClose = () =>{
+   //     tg.close()
+   // }
+   // const onToggleButton = () => {
+        if(tg.MainButton.isVisible){
+            tg.MainButton.show()
+        } else {}
+        return {
+            onClose, tg, user: tg.initDataUnsafe?.user,
+        }
+   // }
 
-
-}
+//}
