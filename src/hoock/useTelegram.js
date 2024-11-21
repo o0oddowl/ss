dfe1323ae2bc7d9043414dd91779d78392bb0f88
o0oddowl <artemtop18@gmail.com>
2024-11-21 16:55:40 +0200

    xss

--- src/hoock/useTelegram.js
+++ src/hoock/useTelegram.js
@@ -4,12 +4,12 @@
    //     tg.close()
    // }
    // const onToggleButton = () => {
-        if(tg.MainButton.isVisible){
-            tg.MainButton.show()
-        } else {}
-        return {
-            onClose, tg, user: tg.initDataUnsafe?.user,
-        }
+      //  if(tg.MainButton.isVisible){
+    //        tg.MainButton.show()
+     //   } else {}
+   //     return {
+    //        onClose, tg, user: tg.initDataUnsafe?.user,
+     //   }
    // }
 
 //}
